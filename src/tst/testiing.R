56169a1de9133fb3409b5535dc8827724f1d3d17
Gabor Bernat <gabor.bernat@gravityrd.com>
2013-04-26 22:22:51 +0200

    Added draft for videos of the 3rd week

--- src/tst/testiing.R
+++ src/tst/testiing.R
@@ -1,9 +1,5 @@
-lifedata = read.table('LifeExpRegion.txt')
-colnames(lifedata) = c('Country', 'LifeExp', 'Region')
-attach(lifedata)
-lifedata[Region=='EAP', ]
-lifesplit = split(lifedata, Region)
-lifeEAP = lifedata[Region=='EAP',]
-lifeSSA = lifedata[Region == 'SSA', ]
-boxplot(lifeEAP[,2], lifeSSA[,2], range=0, border=rainbow(2), names=c('EAP', 'SSA'), main="Life Expectancies: Box Plot")
-boxplot(LifeExp~Region, range=0, border=rainbow(6), main='Life Expectancies: Box Plot (all 6 regions)')
+x = matrix(c(43,44,9,4), 2,2,byrow=TRUE)
+x
+library(gmodels)
+z=CrossTable(x)
+z
